1df86f3838614e0c24b22504d34c6459c953e5d4
omrobbie <putu.roby@yahoo.com>
2018-12-01 22:24:14 +0700

    show the moving monster on the screen

--- NinjaAttack/GameScene.swift
+++ NinjaAttack/GameScene.swift
@@ -36,5 +36,34 @@ class GameScene: SKScene {
     player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
     
     addChild(player)
+    
+    run(SKAction.repeatForever(SKAction.sequence([
+      SKAction.run(addMonster),
+      SKAction.wait(forDuration: 1.0)
+      ])
+    ))
+  }
+  
+  func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+  
+  func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+  }
+  
+  func addMonster() {
+    let monster = SKSpriteNode(imageNamed: "monster")
+    let actualY = random(min: monster.size.height / 2, max: size.height - (monster.size.height / 2))
+    
+    monster.position = CGPoint(x: size.width + (monster.size.width / 2), y: actualY)
+    
+    addChild(monster)
+    
+    let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+    let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width / 2, y: actualY), duration: TimeInterval(actualDuration))
+    let actionMoveDone = SKAction.removeFromParent()
+    
+    monster.run(SKAction.sequence([actionMove, actionMoveDone]))
   }
 }
