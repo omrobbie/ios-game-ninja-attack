581dbd16082111be5ff4322a7ef4341e3632dc4b
omrobbie <putu.roby@yahoo.com>
2018-12-01 21:57:47 +0700

    connect to the scene

--- NinjaAttack/GameViewController.swift
+++ NinjaAttack/GameViewController.swift
@@ -34,6 +34,16 @@ class GameViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
+    
+    scene.scaleMode = .resizeFill
+
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    
+    skView.presentScene(scene)
   }
   
   override var prefersStatusBarHidden: Bool {
